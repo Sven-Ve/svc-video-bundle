1b1740bc6fcb372ef2768ec9c3a7a3ae5b68a77e
Sven Vetter <git.09o@svenvetter.com>
2021-06-03 14:05:12 +0200

    translations

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ Versions
 * Version 1.0.0: First released bundle to satis
 * Version 1.0.1: added translation
 * Version 1.1.0: first sv-video deploy to prod
+* Version 1.1.1: translations
--- bin/release.php
+++ bin/release.php
@@ -1,8 +1,8 @@
 #!/usr/bin/env php
 <?php
 
-$version = "1.1.0";
-$message = "first sv-video deploy to prod";
+$version = "1.1.1";
+$message = "translations";
 
 file_put_contents("README.md", "\n* Version " . $version . ": " . $message, FILE_APPEND);
 
